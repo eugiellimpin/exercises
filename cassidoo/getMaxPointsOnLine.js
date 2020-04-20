acffed38fea64b31848b233066b3bb6d9aba1f15
Eugie Limpin <eugiellimpin@gmail.com>
2020-04-20 21:11:41 +0800

    Add new exercise

---
+++ cassidoo/getMaxPointsOnLine.js
@@ -0,0 +1,7 @@
+// Given n points on a 2D plane, find the maximum number of points that lie on
+// the same straight line.
+
+// Example:
+
+// $ maxPointsOnLine([[1,1],[3,2],[5,3],[4,1],[2,3],[1,4]])
+// $ 4
